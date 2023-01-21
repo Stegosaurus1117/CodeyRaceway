3e2a9d211426a7b89adfc2a466374f662157cabf
Stegosaurus1117 <stegosaurus1117@gmail.com>
2023-01-21 13:01:05 -0500

    added obstacles + timer

---
+++ GM - CodeyRaceway/Assets/Scripts/TimersCountdown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimersCountdown : MonoBehaviour
+{
+    public Text lapTime;
+    public Text startCountdown;
+
+    public float totalLapTime;
+    public float totalCountdownTime;
+    public CodeyMove cm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalCountdownTime >= 0)
+        {
+            cm.Speed = 0;
+            totalCountdownTime -= Time.deltaTime;
+            startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+        }
+        if(totalCountdownTime <= 0)
+        {
+            startCountdown.text = "";
+            totalLapTime -= Time.deltaTime;
+            lapTime.text = Mathf.Round(totalLapTime).ToString();
+            cm.Speed = 90;
+        }
+        if (totalLapTime <= 0)
+        {
+            print("Time's up!");
+        }
+       
+        
+        
+    }
+}
--- GM - CodeyRaceway/Assets/Scripts/TriggerFinishLine.cs
+++ GM - CodeyRaceway/Assets/Scripts/TriggerFinishLine.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class TriggerFinishLine : MonoBehaviour
 {
+    public CheckpointCounter checkpointTracker;
+    public Rigidbody avatar;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("You Win!");
+            if (checkpointTracker.triggeredCheckpoints == checkpointTracker.numberOfCheckpoints)
+            {
+                Debug.Log("You Win!");
+            }
+            else
+            {
+                print("Cheater!");
+                //avatar.mass = 1;
+                //avatar.drag = 0;
+                //avatar.AddForce(1000, 5000, 3000, ForceMode.Impulse);
+                
+                
+            }
+            
         }
     }
 }
