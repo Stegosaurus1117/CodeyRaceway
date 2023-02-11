54f9263b7f81ee18e550846937c63d8807306f4b
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-02-11 13:06:03 -0500

    added boxes

---
+++ GM - CodeyRaceway/Assets/Scripts/itemBoxSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxSpawner : MonoBehaviour
+{
+ 
+    public GameObject itemBox;
+    public int numberOfBoxes;
+
+    public int modifyXPosition;
+    public int modifyZPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnBoxes();
+    }
+
+    private void Update()
+    {
+        
+    }
+    private void spawnBoxes()
+    {
+        for (int i = 0; i < numberOfBoxes; i++)
+        {
+            Vector3 name = new Vector3(transform.position.x + modifyXPosition * i, transform.position.y, transform.position.z + modifyZPosition * i);
+            GameObject itemBoxClone = Instantiate(itemBox, name, transform.rotation);
+
+           
+        }
+    }
+}
